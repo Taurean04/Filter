d90f23d6053914514f73ebc9948de0e6b092d42f
taurean04 <ayobamitomi@gmail.com>
2019-12-10 15:06:31 +0100

    modal update

--- js/action.js
+++ js/action.js
@@ -1,6 +1,14 @@
-const btns = document.querySelectorAll('.btn');
+const btns = document.querySelectorAll('.filter-btn');
 const storeItems = document.querySelectorAll('.store-item');
 const searchBox = document.getElementById('search-item');
+const lBCon = document.querySelector('.lightbox-container');
+const lBItem = document.querySelector('.lightbox-item');
+const images = document.querySelectorAll('.store-img');
+const lBBtn = document.querySelectorAll('.lightbox-control');
+const lBClose = document.querySelector('.lightbox-close');
+
+let imageList = [];
+let imgCount = 0;
 
 btns.forEach(btn => {
     btn.addEventListener('click', e => {
@@ -29,4 +37,39 @@ searchBox.addEventListener('keyup', e => {
             item.style.display = 'none'
         }
     })
-})
+});
+
+images.forEach(image => {
+    imageList.push(image.src);
+});
+
+storeItems.forEach(item => {
+    item.addEventListener('click', e => {
+        let image = e.target.src;
+        lBItem.style.backgroundImage = `url(${image})`;
+        lBCon.classList.add('show');
+        imgCount = imageList.indexOf(image);
+    });
+});
+
+lBBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(btn.classList.contains('btnLeft')){
+            imgCount--;
+            if(imgCount < 0){
+                imgCount = imageList.length - 1;
+            }
+        }
+        if(btn.classList.contains('btnRight')){
+            imgCount++;
+            if(imgCount >= imageList.length){
+                imgCount = 0;
+            }
+        }
+        lBItem.style.backgroundImage = `url(${imageList[imgCount]})`;
+    });
+});
+
+lBClose.addEventListener('click', () => {
+    lBCon.classList.remove('show');
+});
